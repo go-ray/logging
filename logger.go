58a6d9750cba0a303c2f663c5b29779f75b67462
chenshuijin <785795635@qq.com>
2018-07-10 11:26:49 +0800

    fix bug

--- logger.go
+++ logger.go
@@ -65,97 +65,97 @@ func InitLogger(logpath, name, level, format string) {
 }
 
 func Debugf(format string, args ...interface{}) {
-	logger.Debugf(format, args)
+	logger.Debugf(format, args...)
 }
 
 func Infof(format string, args ...interface{}) {
-	logger.Infof(format, args)
+	logger.Infof(format, args...)
 }
 
 func Printf(format string, args ...interface{}) {
-	logger.Printf(format, args)
+	logger.Printf(format, args...)
 }
 
 func Warnf(format string, args ...interface{}) {
-	logger.Warnf(format, args)
+	logger.Warnf(format, args...)
 }
 
 func Warningf(format string, args ...interface{}) {
-	logger.Warningf(format, args)
+	logger.Warningf(format, args...)
 }
 
 func Errorf(format string, args ...interface{}) {
-	logger.Errorf(format, args)
+	logger.Errorf(format, args...)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	logger.Fatalf(format, args)
+	logger.Fatalf(format, args...)
 }
 
 func Panicf(format string, args ...interface{}) {
-	logger.Panicf(format, args)
+	logger.Panicf(format, args...)
 }
 
 func Debug(args ...interface{}) {
-	logger.Debug(args)
+	logger.Debug(args...)
 }
 
 func Info(args ...interface{}) {
-	logger.Info(args)
+	logger.Info(args...)
 }
 
 func Print(args ...interface{}) {
-	logger.Print(args)
+	logger.Print(args...)
 }
 
 func Warn(args ...interface{}) {
-	logger.Warn(args)
+	logger.Warn(args...)
 }
 
 func Warning(args ...interface{}) {
-	logger.Warning(args)
+	logger.Warning(args...)
 }
 
 func Error(args ...interface{}) {
-	logger.Error(args)
+	logger.Error(args...)
 }
 
 func Fatal(args ...interface{}) {
-	logger.Fatal(args)
+	logger.Fatal(args...)
 }
 
 func Panic(args ...interface{}) {
-	logger.Panic(args)
+	logger.Panic(args...)
 }
 
 func Debugln(args ...interface{}) {
-	logger.Debugln(args)
+	logger.Debugln(args...)
 }
 
 func Infoln(args ...interface{}) {
-	logger.Infoln(args)
+	logger.Infoln(args...)
 }
 
 func Println(args ...interface{}) {
-	logger.Println(args)
+	logger.Println(args...)
 }
 
 func Warnln(args ...interface{}) {
-	logger.Warnln(args)
+	logger.Warnln(args...)
 }
 
 func Warningln(args ...interface{}) {
-	logger.Warningln(args)
+	logger.Warningln(args...)
 }
 
 func Errorln(args ...interface{}) {
-	logger.Errorln(args)
+	logger.Errorln(args...)
 }
 
 func Fatalln(args ...interface{}) {
-	logger.Fatalln(args)
+	logger.Fatalln(args...)
 }
 
 func Panicln(args ...interface{}) {
-	logger.Panicln(args)
+	logger.Panicln(args...)
 }
